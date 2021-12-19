4d73c8e360f826990ddfe78be3d55a37db9bb3c3
Hanfei Shen <qqshfox@gmail.com>
2021-12-18 23:04:33 -0800

    [settings/java] add indent config

---
+++ lua/settings/java.lua
@@ -0,0 +1,4 @@
+---@diagnostic disable: undefined-global
+vim.api.nvim_command([[autocmd FileType java set tabstop=4]])
+vim.api.nvim_command([[autocmd FileType java set softtabstop=4]])
+vim.api.nvim_command([[autocmd FileType java set shiftwidth=4]])
