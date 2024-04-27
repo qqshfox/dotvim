efc687c1da150cbd8c35a86c76cfd9a5d944efbf
Hanfei Shen <qqshfox@gmail.com>
2024-04-26 19:01:37 -0700

    [treesitter] remove help

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -28,7 +28,7 @@ return require('packer').startup(function(use)
     run = ':TSUpdate',
     config = function()
       require'nvim-treesitter.configs'.setup {
-        ensure_installed = { "lua", "vim", "vimdoc", "help", "java", "python" },
+        ensure_installed = { "lua", "vim", "vimdoc", "java", "python" },
         highlight = { enable = true },
       }
     end
