cf50695707e1e6a4c48daea9dcd79c138c6efe35
Hanfei Shen <qqshfox@gmail.com>
2024-04-27 13:35:13 -0700

    [cmp] fix tab

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,16 +1,17 @@
 ---@diagnostic disable: undefined-global
 return function()
-  local cmp = require 'cmp'
-  local lspkind = require 'lspkind'
-  local keymap = require 'cmp.utils.keymap'
-
-  vim.opt.completeopt:remove 'longest'
-
-  local check_back_space = function()
-    local col = vim.fn.col('.') - 1
-    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s')
+  local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
   end
 
+  local feedkey = function(key, mode)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+  end
+
+  local cmp = require 'cmp'
+  local lspkind = require 'lspkind'
   cmp.setup {
     formatting = {
       format = function(entry, vim_item)
@@ -36,32 +37,33 @@ return function()
     },
     mapping = {
       ['<Tab>'] = cmp.mapping(function(fallback)
-        if vim.fn.pumvisible() == 1 then
-          vim.fn.feedkeys(keymap.t '<C-n>', 'n')
-        elseif vim.fn['vsnip#available']() == 1 then
-          vim.fn.feedkeys(keymap.t '<Plug>(vsnip-expand-or-jump)', '')
-        elseif check_back_space() then
-          vim.fn.feedkeys(keymap.t '<Tab>', 'n')
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif vim.fn['vsnip#available'](1) == 1 then
+          feedkey('<Plug>(vsnip-expand-or-jump)', '')
+        elseif has_words_before() then
+          cmp.complete()
         else
-          fallback()
+          fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
         end
-      end, {
-        'i',
-        's',
-      }),
-      ['<S-Tab>'] = cmp.mapping(function(fallback)
-        if vim.fn.pumvisible() == 1 then
-          vim.fn.feedkeys(keymap.t '<C-p>', 'n')
-        elseif vim.fn['vsnip#available']() == 1 then
-          vim.fn.feedkeys(keymap.t '<Plug>(vsnip-jump-prev)', '')
-        elseif check_back_space() then
-          vim.fn.feedkeys(keymap.t '<S-Tab>', 'n')
-        else
-          fallback()
+      end, { 'i', 's' }),
+      ['<S-Tab>'] = cmp.mapping(function()
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif vim.fn['vsnip#jumpable'](-1) == 1 then
+          feedkey('<Plug>(vsnip-jump-prev)', '')
         end
-      end, {
-        'i',
-        's',
+      end, { 'i', 's' }),
+      ['<CR>'] = cmp.mapping({
+        i = function(fallback)
+          if cmp.visible() and cmp.get_active_entry() then
+            cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+          else
+            fallback()
+          end
+        end,
+        s = cmp.mapping.confirm({ select = true }),
+        c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
       }),
     },
     sources = {
